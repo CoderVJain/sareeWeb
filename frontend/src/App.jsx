d9af838a74017b77bef44fc145d8328f254b235f
Varun Jain <vj986725@gmail.com>
2025-12-10 10:19:32 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx"
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
 import ProductCatalogue from "./components/Products/ProductCatalogue.jsx";
 import ProductInfo from "./components/Products/ProductInfo.jsx";
